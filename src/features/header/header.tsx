7c81fc55349b36463efe2182043345aa4d4c104c
Aina Fitia <ainafitia120868@gmail.com>
2025-04-03 00:54:34 -0700

    header without fonctionnality

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -1,21 +1,20 @@
 import { MenuHeader } from "./menu";
 
-
-
 const Header = () => {
-    return ( 
-        <header>
-            <div className="h-24 bg-[#DCE4EF] px-48 flex items-center justify-end"> 
-                <img src="" alt="TEst" />
-                <div className="w-full"/>
-                <MenuHeader title="Accueil"/>
-                <MenuHeader title="Services"/>
-                <MenuHeader title="Projets"/>
-                <MenuHeader title="Technologies"/>
-                <MenuHeader title="Contact"/>
-            </div>
-        </header>
-     );
-}
- 
+  return (
+    <header>
+      <div className="h-24 bg-[#dce4ef66] px-48 flex items-center justify-end">
+        <img src="assets/AfoLogoMobile.webp" alt="" />
+        <img src="assets/AfoLogoDesk.webp" alt="afo" />
+        <div className="w-full" />
+        <MenuHeader title="Accueil" />
+        <MenuHeader title="Services" />
+        <MenuHeader title="Projets" />
+        <MenuHeader title="Technologies" />
+        <MenuHeader title="Contact" />
+      </div>
+    </header>
+  );
+};
+
 export default Header;
