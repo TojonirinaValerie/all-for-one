3ff4476b61dc7d721920d7677fc7163013123dc6
tojonirina <tsilavinatojo01@gmail.com>
2025-04-03 09:28:56 +0300

    initialisation

---
+++ src/components/section.tsx
@@ -0,0 +1,10 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+const Section = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <section ref={ref} className={cn("px-[150px] py-[80px]", className)} {...props} />
+));
+Section.displayName = "Section";
