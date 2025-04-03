d118778374e6c95167cb443e264e829e052b2f54
tojonirina <tsilavinatojo01@gmail.com>
2025-04-03 09:55:12 +0300

    section choose us init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ChooseUs from "@/features/choise-us/choose-us";
 import Header from "@/features/header/header";
 
 export default function Home() {
   return (
     <main>
       <Header/>
+      <ChooseUs />
     </main>
   );
 }
---
+++ src/features/choise-us/choose-us.tsx
@@ -0,0 +1,21 @@
+import { Section } from "@/components/section";
+
+const ChooseUs = () => {
+  return (
+    <Section className="flex flex-col items-center text">
+      <div className="flex flex-col items-center">
+        <h2 className="text-foreground/50">WHY CHOOSE US</h2>
+        <h1>
+          Pourquoi nous <span>choisir</span> ?
+        </h1>
+        <p>
+          Lorem ipsum dolor a amet, consectetur adipiscing elit. Vestibulum
+          feugiat pretium velit ac scelerisque. In vitae ligula eget ligula
+          pulvinar rutrum convallis id.{" "}
+        </p>
+      </div>
+    </Section>
+  );
+};
+
+export default ChooseUs;
