6ca991afb0e7024ed4c8233d37836a3941e1db33
Aina Fitia <ainafitia120868@gmail.com>
2025-04-04 02:52:36 -0700

    responsive Hero

--- src/features/hero/hero.tsx
+++ src/features/hero/hero.tsx
@@ -5,7 +5,7 @@ import { TextCenter } from "./textCenter";
 export function Hero() {
     return (
       <div style={{ backgroundImage: "url('assets/bg_afo.webp')", backgroundSize: "cover", height: "100vh", width: "100%" }}>
-        <Section className="flex justify-between h-full">
+        <Section className="flex flex-row justify-between not-xl:flex-col not-xl:gap-5 not-xl:justify-center h-full ">
             <IconLeft/>
             <TextCenter/>
             <div className=" w-12 "/>
--- src/features/hero/icon.tsx
+++ src/features/hero/icon.tsx
@@ -1,12 +1,26 @@
+'use client'
+
+import { useState } from "react";
+
 interface IconCompoProps {
   src: string;
+  hoverSrc: string;
   onClick?:()=>void;
 }
 
-export function IconCompo({ src, onClick }: Readonly<IconCompoProps>) {
+export function IconCompo({ src, onClick , hoverSrc }: Readonly<IconCompoProps>) {
+
+  const [iconSrc, setIconSrc] = useState<string>(src);
+
+  const handleMouseEnter = () => setIconSrc(hoverSrc);
+  const handleMouseLeave = () => setIconSrc(src);
+
   return (
-    <div className="h-12 w-12 rounded-full content-center hover:bg-primary hover:text-white">
-      <img src={src} />
+    <div className="h-12 w-12 rounded-full content-center hover:bg-primary hover:text-white" 
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
+    >
+      <img src={iconSrc} className="filter transition duration-300  hover:grayscale-0" />
     </div>
   );
 }
