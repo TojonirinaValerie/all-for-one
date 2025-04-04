6cfe6b15a02f0b07a66b2bb7d4098e40f355127c
Aina Fitia <ainafitia120868@gmail.com>
2025-04-04 07:08:53 -0700

    header final

--- src/features/header/menu.tsx
+++ src/features/header/menu.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 
 interface MenuHeaderProps{
     title:string;
     link:string;
+    active:boolean;
+    onClick?: () => void;
 }
 
-export function MenuHeader({title, link}:MenuHeaderProps){
-    return <a href={link} className="h-full px-5 text-xs 2xl:text-md font-[5px] justify-center content-center hover:font-medium text-primary hover:border-b-2 border-b-primary">
+export function MenuHeader({title, link, active , onClick}:MenuHeaderProps){
+    return <Link href={link} onClick={onClick} className={`h-full px-5 text-xs 2xl:text-md font-[5px] justify-center content-center hover:font-medium ${active?"font-medium border-b-2":"font-light"} text-primary hover:border-b-2 transition-all duration-150 border-b-primary`}>
         {title}
-    </a>
+    </Link>
 }
--- src/features/hero/hero.tsx
+++ src/features/hero/hero.tsx
@@ -4,19 +4,20 @@ import { TextCenter } from "./textCenter";
 
 export function Hero() {
   return (
-    <div
+    <section
       style={{
         backgroundImage: "url('assets/bg_afo.webp')",
         backgroundSize: "cover",
         height: "100vh",
         width: "100%",
       }}
+      id="accueil"
     >
-      <Section className="flex flex-row justify-between not-xl:flex-col not-xl:gap-5 not-xl:justify-center h-full pt-32" id="accueil">
+      <Section className="flex flex-row justify-between not-xl:flex-col not-xl:gap-5 not-xl:justify-center h-full pt-32" >
         <IconLeft />
         <TextCenter />
         <div className=" w-12 " />
       </Section>
-    </div>
+    </section>
   );
 }
