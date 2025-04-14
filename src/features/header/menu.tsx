7d75d91d9415e0c6d5ba7982c09615eeb5fca0a7
unknown <ainafitia120868@gmail.com>
2025-04-14 10:59:12 +0200

    Header left drawer

--- src/features/header/menu.tsx
+++ src/features/header/menu.tsx
@@ -8,7 +8,26 @@ interface MenuHeaderProps{
 }
 
 export function MenuHeader({title, link, active , onClick}:MenuHeaderProps){
-    return <Link href={link} onClick={onClick} className={`h-full px-5 text-xs 2xl:text-md font-[5px] justify-center content-center hover:font-medium ${active?"font-medium border-b-2":"font-light"} text-primary hover:border-b-2 transition-all duration-150 border-b-primary`}>
-        {title}
-    </Link>
+    return <Link
+    href={link}
+    onClick={onClick}
+    className={`
+      h-full
+      w-full md:w-auto
+      px-4 md:px-5
+      py-2 md:py-0
+      text-sm sm:text-base lg:text-[15px] xl:text-[16px] 2xl:text-md
+      font-medium
+      text-primary
+      hover:font-semibold
+      hover:border-b-2
+      border-b-primary
+      transition-all duration-150
+      ${active ? "font-semibold border-b-2" : "font-light border-b-0"}
+      flex items-center md:justify-center
+    `}
+  >
+    {title}
+  </Link>
+  
 }
